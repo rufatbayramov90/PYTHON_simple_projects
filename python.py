b11c5a195218f6ec1d33cea063158314a7f34283
rufatbayramov90 <rufatbyrmv@gmail.com>
2023-09-25 09:20:35 +0400

    writing of single digits

--- python.py
+++ python.py
@@ -1,5 +1,12 @@
-number = 0
+'''number = 0
 while number<8:
     print(number)
     number = number + 1
     print("Transaction completed")
+'''
+
+number = 0 
+while number <10:
+    if number % 2 == 0:
+        print(number)
+    number += 1 
